e2c658b68c15674cd4b262988b13ad6beeb7c13d
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-09-26 19:08:30 -0400

    add OP_CONSTANT dispatcher

--- vm.c
+++ vm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -11,15 +13,26 @@ void freeVM() {
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
-      case OP_RETURN: {
-        return INTERPRET_OK;
-      }
+      case OP_CONSTANT:
+        {
+          Value constant = READ_CONSTANT();
+          printValue(constant);
+          printf("\n");
+          break;
+        }
+      case OP_RETURN:
+        {
+          return INTERPRET_OK;
+        }
     }
   }
+#undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk* chunk) {
