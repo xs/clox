ce1de8e75d608d2b58eaa226e3f8b2ed1bf38253
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-09-27 16:12:27 -0400

    add binary operators to vm/debug

--- vm.c
+++ vm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
@@ -20,6 +21,12 @@ void freeVM() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -37,27 +44,26 @@ static InterpretResult run() {
 #endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
-      case OP_CONSTANT:
-        {
-          Value constant = READ_CONSTANT();
-          push(constant);
-          break;
-        }
-      case OP_NEGATE:
-        {
-          push(-pop());
-          break;
-        }
-      case OP_RETURN:
-        {
-          printValue(pop());
-          printf("\n");
-          return INTERPRET_OK;
-        }
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        push(constant);
+        break;
+      }
+      case OP_ADD:      BINARY_OP(+); break;
+      case OP_SUBTRACT: BINARY_OP(-); break;
+      case OP_MULTIPLY: BINARY_OP(*); break;
+      case OP_DIVIDE:   BINARY_OP(/); break;
+      case OP_NEGATE:   push(-pop()); break;
+      case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
+        return INTERPRET_OK;
+      }
     }
   }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
