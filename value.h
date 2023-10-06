0d90921f82e34be5688069d5e70227ec3e105a82
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-10-06 14:34:12 -0400

    ch 19.1; 19.2

--- value.h
+++ value.h
@@ -3,10 +3,14 @@
 
 #include "common.h"
 
+typedef struct Obj Obj;
+typedef struct ObjString ObjString;
+
 typedef enum {
   VAL_BOOL,
   VAL_NIL,
   VAL_NUMBER,
+  VAL_OBJ,
 } ValueType;
 
 typedef struct {
@@ -14,13 +18,17 @@ typedef struct {
   union {
     bool boolean;
     double number;
+    Obj* obj;
   } as;
 } Value;
 
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)
 #define IS_NIL(value)     ((value).type == VAL_NIL)
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
+// pulls out the obj from a value
+#define AS_OBJ(value)    ((value).as.obj)
 // pulls out the boolean from a value
 #define AS_BOOL(value)    ((value).as.boolean)
 // pulls out the number from a value
@@ -32,6 +40,8 @@ typedef struct {
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 // creates a value from a number
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+// creates a value from an object
+#define OBJ_VAL(object)    ((Value){VAL_OBJ, {.obj = (Obj*)object}})
 
 typedef struct {
   int capacity;
