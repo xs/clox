e9110c4974f068e410ec0699d548a6e6c572272c
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-10-13 13:02:13 -0400

    table allocation/resize

--- table.c
+++ table.c
@@ -6,13 +6,80 @@
 #include "table.h"
 #include "value.h"
 
-void initTable(Table *table) {
+#define TABLE_MAX_LOAD 0.75
+
+void initTable(Table* table) {
   table->count = 0;
   table->capacity = 0;
   table->entries = NULL;
 };
 
-void freeTable(Table *table) {
+void freeTable(Table* table) {
   FREE_ARRAY(Entry, table->entries, table->capacity);
   initTable(table);
+}
+
+static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
+  uint32_t index = key->hash % capacity;
+  for (;;) {
+    Entry* entry = &entries[index];
+    if (entry->key == key || entry->key == NULL) {
+      return entry;
+    }
+
+    // linear probing: keep looking forward until
+    // key matches or we hit an empty bucket
+    index = (index + 1) % capacity;
+  }
+}
+
+static void adjustCapacity(Table* table, int capacity) {
+  // prepare a new clean allocated table with future capacity
+  Entry* entries = ALLOCATE(Entry, capacity);
+  for (int i = 0; i < capacity; i++) {
+    entries[i].key = NULL;
+    entries[i].value = NIL_VAL;
+  }
+
+  // for present capacity, figure out the new table's buckets for current keys
+  for (int i = 0; i < table->capacity; i++) {
+    Entry* entry = &table->entries[i];
+    if (entry->key == NULL) continue;
+
+    Entry* dest = findEntry(entries, capacity, entry->key);
+    dest->key = entry->key;
+    dest->value = entry->value;
+  }
+
+  // release the old table
+  FREE_ARRAY(Entry, table->entries, table->capacity);
+  table->entries = entries;
+  table->capacity = capacity;
+}
+
+// sets key/value in the table; returns true if new value is written
+bool tableSet(Table* table, ObjString* key, Value value) {
+  if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
+    int capacity = GROW_CAPACITY(table->capacity);
+    adjustCapacity(table, capacity);
+  }
+
+  // findEntry either finds the actual key's bucketj or the first empty bucket
+  Entry* entry = findEntry(table->entries, table->capacity, key);
+  bool isNewKey = entry->key == NULL;
+  if (isNewKey) table->count++;
+
+  entry->key = key;
+  entry->value = value;
+  return isNewKey;
+}
+
+// it's kind of like a ruby Hash.merge
+void tableAddAll(Table* from, Table* to) {
+  for (int i = 0; i < from->capacity; i++) {
+    Entry* entry = &from->entries[i];
+    if (entry->key != NULL) {
+      tableSet(to, entry->key, entry->value);
+    }
+  }
 }
